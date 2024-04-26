806e5a3ae1361b5ab3f230189b38eb760f525bb6
RiiXXD <thereforeme06@gmail.com>
2024-04-26 13:33:07 +0530

    working on Static UI

--- weather/src/Components/Home.jsx
+++ weather/src/Components/Home.jsx
@@ -7,12 +7,23 @@ import {
   Flex,
   Button,
   Box,
+  Heading,
+  Text,
 } from "@chakra-ui/react";
 import { FaSearchLocation } from "react-icons/fa";
 import { FaLocationCrosshairs } from "react-icons/fa6";
+import { CiLocationOn } from "react-icons/ci";
 const Home = () => {
+  const dateBuilder = () => {
+    const today = new Date();
+    return today.toLocaleDateString(undefined, {
+      weekday: "short",
+      day: "numeric",
+      month: "short",
+    });
+  };
   return (
-    <Container w="80% ">
+    <Container w="90%" minH="70vh" p="2em">
       <Flex
         width={"100%"}
         className="SearchBox"
@@ -32,7 +43,19 @@ const Home = () => {
           <FaLocationCrosshairs />
         </Button>
       </Flex>
-      <Box mt="1em" bg="rgba(173, 216, 230, 0.5)"></Box>
+      <Box mt="1em" bg="rgba(173, 216, 230, 0.1)" textAlign={"center"} p="2em">
+        <Heading>34 &#176;</Heading>
+        <Text>Haze</Text>
+        <Flex align={"center"} justify={"center"} gap={"10PX"}>
+          <Text>Today •</Text>
+
+          <Text>{dateBuilder()}</Text>
+        </Flex>
+        <Flex align={"center"} justify={"center"} gap={"10PX"}>
+          <CiLocationOn />
+          <Text>New Delhi</Text>
+        </Flex>
+      </Box>
     </Container>
   );
 };
